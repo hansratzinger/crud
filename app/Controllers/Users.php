446a551bb9cfc477c83229f8fee9d169c297b4dd
Hans Ratzinger <hans@ratzinger.com>
2020-10-14 19:52:25 +0200

    AdminLTE Seite erscheint erstmalig bei Aufruf von 10.0.0.30:8084/users / Tutorial #1 37:38 AutoCRUD https://www.youtube.com/watch?v=q39sfK1gOGE

---
+++ app/Controllers/Users.php
@@ -0,0 +1,14 @@
+<?php namespace App\Controllers;
+
+class Users extends BaseController
+{
+	public function index()
+	{
+		$data['title'] = 'Users';
+		$data['table'] = '';
+		return view('admin/users/table',$data);
+	}
+
+	//--------------------------------------------------------------------
+
+}
